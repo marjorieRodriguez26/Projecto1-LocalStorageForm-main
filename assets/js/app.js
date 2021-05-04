6690ebc3878668d096780ed1b927b954ea576954
MarjorieRodriguez <rodzmarjmaj@gmail.com>
2021-05-04 10:25:33 +0800

    Addingg new elements to the DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,31 @@ function eventListerners(){
 //Functions
 function newTweet(e){
 e.preventDefault();
+// read the textarea value
+const tweet = document.getElementById('tweet').value;
+
+// create the  remove button
+const removeBtn = document.createElement('a');
+removeBtn.classList = 'remove-tweet';
+removeBtn.textContent = 'X';
+
+
+// create an <li> element
+const li = document.createElement('li');
+li.textContent = tweet;
+
+
+// Add the remove button to each tweet 
+li.appendChild(removeBtn);
+
+// Add to the List
+tweetList.appendChild(li);
+//add to local storage
+addTweetLocalStorage(tweet);
+
+//print the alert
+alert('Tweet Added');
+
+this.reset();
+
+}
